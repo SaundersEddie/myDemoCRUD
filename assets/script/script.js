f94ad87bd5917eafa7d529d56716a8a66684738b
Eddie Saunders <saunders.eddie@outlook.com>
2020-11-05 10:04:08 -0500

    CSS styling added for form

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,31 @@
 // EXS 5th November 2020 - edwyn.saunders@outlook.com
 
 console.log ("Script loaded");
+
+function onFormSubmit() {
+    console.log ("Submit Clicked")
+    var formData = readFormDate();
+    console.log (`formData: ${formData}`);
+
+
+
+}
+
+
+function readFormDate() {
+    var formData={};
+    formData["empfullName"] = document.getElementById("empFullName").value;
+    formData["empNumber"] = document.getElementById("empNumber").value;
+    formData["empSalary"] = document.getElementById("empSalary").value;
+    formData["empLocation"] = document.getElementById("empLocation").value;
+    return formData;
+}
+
+// CRUD Operations
+
+function insertNewRecords(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    
+
+}
