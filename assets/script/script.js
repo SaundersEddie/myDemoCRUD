3098f18fc53a07bae165ba5ad3da6935fb3edf7a
Eddie Saunders <saunders.eddie@outlook.com>
2020-11-05 10:36:36 -0500

    submit event created, readdata and post data

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,38 +1,33 @@
 // EXS 5th November 2020 - edwyn.saunders@outlook.com
 
-console.log ("Script loaded");
-
 function onFormSubmit() {
-    console.log ("Submit Clicked")
-    var formData = readFormDate();
-    console.log (`formData: ${formData}`);
-
-
-
+    var formData = readFormData();
+    insertNewRecords(formData);
 }
 
-
-function readFormDate() {
+function readFormData() {
     var formData={};
-    formData["empfullName"] = document.getElementById("empFullName").value;
+    formData["empFullName"] = document.getElementById("empFullName").value;
     formData["empNumber"] = document.getElementById("empNumber").value;
     formData["empSalary"] = document.getElementById("empSalary").value;
     formData["empLocation"] = document.getElementById("empLocation").value;
+    console.log (`FormData: ${formData}`);
     return formData;
 }
 
 // CRUD Operations
-
 function insertNewRecords(data) {
     var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
     cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.empfullName;
-    cell2 = newRow.insertCell(0);
+    cell1.innerHTML = data.empFullName;
+    cell2 = newRow.insertCell(1);
     cell2.innerHTML = data.empNumber;
-    cell3 = newRow.insertCell(0);
+    cell3 = newRow.insertCell(2);
     cell3.innerHTML = data.empSalary;
-    cell4 = newRow.insertCell(0);
+    cell4 = newRow.insertCell(3);
     cell4.innerHTML = data.empLocation;
-
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a>Edit</a><a>Delete</a>`;
 }
+
