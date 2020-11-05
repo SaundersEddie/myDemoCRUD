688d227f4402cc71e27b40b703ef8ae444040438
Eddie Saunders <saunders.eddie@outlook.com>
2020-11-05 10:12:11 -0500

    correct error wioth table heading display in CSS

--- assets/script/script.js
+++ assets/script/script.js
@@ -26,6 +26,13 @@ function readFormDate() {
 function insertNewRecords(data) {
     var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
-    
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.empfullName;
+    cell2 = newRow.insertCell(0);
+    cell2.innerHTML = data.empNumber;
+    cell3 = newRow.insertCell(0);
+    cell3.innerHTML = data.empSalary;
+    cell4 = newRow.insertCell(0);
+    cell4.innerHTML = data.empLocation;
 
 }
